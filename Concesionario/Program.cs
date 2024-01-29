643114e20e3d5cfd121782f2af61d74491bbd0ad
Carmeenrb <carmeenrbu@gmail.com>
2024-01-29 12:04:11 +0100

    Agregar archivos de proyecto.

---
+++ Concesionario/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Concesionario
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           // CONTROLADOR PRINCIPAL DE LA APLICACIÓN
+
+            // MENÚ DE ACCIONES 
+
+            // PROCESO DE LAS ACCIONES
+
+
+        }
+    }
+}
